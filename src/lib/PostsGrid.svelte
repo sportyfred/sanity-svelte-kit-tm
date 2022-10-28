559e234789027967ce968ab8f2dd6fcd3e3ad5f8
sportyfred <ivanboring@gmail.com>
2022-10-28 18:43:01 +0200

    Kdnd
    Mznz

--- src/lib/PostsGrid.svelte
+++ src/lib/PostsGrid.svelte
@@ -10,11 +10,11 @@
   {/each}
 </section>
 
-<!--<style>
+<style>
   section {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-template-columns: 2fr 1fr;
     gap: 3rem 2rem;
     margin-top: 3rem;
   }
-</style>-->
+</style>
