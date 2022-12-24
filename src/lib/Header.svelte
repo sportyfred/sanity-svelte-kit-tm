1b03496dfed4c39c5e4d5b91df9a2accbb5e98df
sportyfred <ivanboring@gmail.com>
2022-12-24 14:16:54 +0100

    Update Header.svelte

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,6 +5,12 @@
   export let blogTitle = 'tm'
 
 
+import ImageBlock from '$lib/ImageBlock.svelte'
+  import AuthorBlock from '$lib/AuthorBlock.svelte'
+
+import SanityImage from '$lib/SanityImage.svelte'
+  export let author
+</script>
 
 
  
@@ -12,7 +18,10 @@
 
 <header>
  
-</header>
+
+{#if author.image}
+  <SanityImage image={author.image} />
+{/if} </header>
 
 <style>header{
     text-align: center;
