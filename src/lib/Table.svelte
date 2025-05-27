f2a89ddfed96ca2d42af70225ac2be8ca7d13589
sportyfred <ivanboring@gmail.com>
2025-05-27 22:37:59 +0200

    Create Table.svelte

---
+++ src/lib/Table.svelte
@@ -0,0 +1,87 @@
+<table style="table-layout: auto;">
+<tbody>
+
+
+
+  <th colspan="4">Exhibitions</th>
+
+ {#each author.utstallningar.rows as row}
+
+ 
+
+     {#if row.cells[1]!==''}
+
+ <tr>
+    <td class="year">{row.cells[0]}</td>
+
+   {#if row.cells[4]!==''}
+      <td><a target="_blank" href="{row.cells[4]}">{row.cells[1]}</a></td>
+      
+      {:else}
+        <td>{row.cells[1]}</td>
+
+
+{/if}
+  <td>{row.cells[2]}</td>
+   <td>{row.cells[3]}</td>
+
+  
+  </tr>
+{/if}
+
+  {/each}
+ 
+  <th colspan="4">Publications</th>
+
+ {#each author.publikationer.rows as row}
+ 
+
+
+     {#if row.cells[1]!==''}
+      <tr>
+    <td class="year">{row.cells[0]}</td>
+
+   {#if row.cells[2]!==''}
+      <td colspan="3"><a target="_blank" href="{row.cells[2]}">{row.cells[1]}</a></td>
+      
+      {:else}
+        <td colspan="3">{row.cells[1]}</td>
+
+
+{/if}
+  
+  
+  
+        
+ </tr>
+{/if}
+  
+ 
+  {/each}
+ 
+  <th colspan="4">Grants</th>
+
+ {#each author.stipendier.rows as row}
+ {#if row.cells[1]!==''}
+ <tr>
+     
+    <td class="year">{row.cells[0]}</td>
+
+   {#if row.cells[2]!==''}
+      <td colspan="3"><a target="_blank" href="{row.cells[2]}">{row.cells[1]}</a></td>
+      
+      {:else}
+        <td colspan="3">{row.cells[1]}</td>
+
+
+{/if}
+  
+  
+  
+        
+
+
+  
+  </tr>{/if}
+  {/each}
+ </table>
