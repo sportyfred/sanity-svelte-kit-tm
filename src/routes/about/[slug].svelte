cdba51295607f5c4d5add3638fe541f62effb5c7
sportyfred <ivanboring@gmail.com>
2025-05-27 22:37:05 +0200

    Update [slug].svelte

--- src/routes/about/[slug].svelte
+++ src/routes/about/[slug].svelte
@@ -7,7 +7,7 @@
   import AuthorBlock from '$lib/AuthorBlock.svelte'
   import PostsGrid from '$lib/PostsGrid.svelte'
   import SanityImage from '$lib/SanityImage.svelte'
-
+ import Table from '$lib/Table.svelte'
   export let author
 
     
@@ -32,97 +32,10 @@
     }}
   />
 {/if}
-{#if author.utstallningar}
 
-<table style="table-layout: auto;">
-<tbody>
-
-
-
-  <th colspan="4">Exhibitions</th>
-
- {#each author.utstallningar.rows as row}
-
- 
-
-     {#if row.cells[1]!==''}
-
- <tr>
-    <td class="year">{row.cells[0]}</td>
-
-   {#if row.cells[4]!==''}
-      <td><a target="_blank" href="{row.cells[4]}">{row.cells[1]}</a></td>
-      
-      {:else}
-        <td>{row.cells[1]}</td>
-
-
+{if author.utstallningar}
+<Table />
 {/if}
-  <td>{row.cells[2]}</td>
-   <td>{row.cells[3]}</td>
-
-  
-  </tr>
-{/if}
-
-  {/each}
- 
-  <th colspan="4">Publications</th>
-
- {#each author.publikationer.rows as row}
- 
-
-
-     {#if row.cells[1]!==''}
-      <tr>
-    <td class="year">{row.cells[0]}</td>
-
-   {#if row.cells[2]!==''}
-      <td colspan="3"><a target="_blank" href="{row.cells[2]}">{row.cells[1]}</a></td>
-      
-      {:else}
-        <td colspan="3">{row.cells[1]}</td>
-
-
-{/if}
-  
-  
-  
-        
- </tr>
-{/if}
-  
- 
-  {/each}
- 
-  <th colspan="4">Grants</th>
-
- {#each author.stipendier.rows as row}
- {#if row.cells[1]!==''}
- <tr>
-     
-    <td class="year">{row.cells[0]}</td>
-
-   {#if row.cells[2]!==''}
-      <td colspan="3"><a target="_blank" href="{row.cells[2]}">{row.cells[1]}</a></td>
-      
-      {:else}
-        <td colspan="3">{row.cells[1]}</td>
-
-
-{/if}
-  
-  
-  
-        
-
-
-  
-  </tr>{/if}
-  {/each}
- </table>
-{/if}
-
  <style>
 
  @media 
