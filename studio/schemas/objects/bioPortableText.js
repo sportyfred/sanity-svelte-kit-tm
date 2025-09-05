30beddd534197599081dd33400bdadfc9f72f4ac
sportyfred <ivanboring@gmail.com>
2025-09-05 14:53:48 +0200

    Update bioPortableText.js

--- studio/schemas/objects/bioPortableText.js
+++ studio/schemas/objects/bioPortableText.js
@@ -1,30 +1,37 @@
 export default {
+  title: 'Portable Text',
   name: 'bioPortableText',
   type: 'array',
-  title: 'Excerpt',
   of: [
     {
       type: 'block',
       title: 'Block',
+      // Styles let you set what your user can mark up blocks with. These
+      // corrensponds with HTML tags, but you can set any title or value
+      // you want and decide how you want to deal with it where you want to
+      // use your content.
       styles: [
-        { title: 'Normal', value: 'normal' },
-        { title: 'Small', value: 'small' },
-        { title: 'H1', value: 'h1' },
-        { title: 'H2', value: 'h2' },
-        { title: 'H3', value: 'h3' },
-        { title: 'H4', value: 'h4' },
-        { title: 'Quote', value: 'blockquote' }
+        {title: 'Normal', value: 'normal'},
+         {title: 'Small', value: 'small'},
+          {title: 'H1', value: 'h1'},
+        {title: 'H2', value: 'h2'},
+        {title: 'H3', value: 'h3'},
+        {title: 'H4', value: 'h4'},
+        {title: 'Quote', value: 'blockquote'}
       ],
       lists: [
-        { title: 'Bullet', value: 'bullet' },
-        { title: 'Numbered', value: 'number' }
+        {title: 'Bullet', value: 'bullet'},
+        {title: 'Numbered', value: 'number'}
       ],
+      // Marks let you mark up inline text in the block editor.
       marks: {
+        // Decorators usually describe a single property – e.g. a typographic
+        // preference or highlighting by editors.
         decorators: [
-          { title: 'Strong', value: 'strong' },
-          { title: 'Emphasis', value: 'em' },
-          { title: 'Code', value: 'code' }
+          {title: 'Strong', value: 'strong'},
+          {title: 'Emphasis', value: 'em'}
         ],
+        // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
           {
             name: 'link',
@@ -39,19 +46,25 @@ export default {
             ]
           }
         ]
-      }
+      },
+      of: [{type: 'authorReference'}]
     },
+    // You can add additional types here. Note that you can't use
+    // primitive types such as 'string' and 'number' in the same array
+    // as a block type.
     {
-      name: 'soundcloud',
-      type: 'object',
-      title: 'SoundCloud Embed',
-      fields: [
-        {
-          name: 'soundcloudurl',
-          type: 'url',
-          title: 'Soundcloud URL'
-        }
-      ]
+      type: 'image',
+      options: {hotspot: true}
+    },
+    {type: 'youtube'},
+
+    
+    {type: 'soundcloud'},
+{type: 'vimeo'},
+
+    
+    {
+      type: 'code'
     }
   ]
 }
