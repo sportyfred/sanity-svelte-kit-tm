71c5e42558b1afa0613e179c9475ef9675913174
sportyfred <ivanboring@gmail.com>
2025-09-08 21:44:50 +0200

    Update SoundCloudBlock.svelte

--- src/lib/SoundCloudBlock.svelte
+++ src/lib/SoundCloudBlock.svelte
@@ -8,10 +8,11 @@
  
 </script>
 
-
-
+<iframe width="100%" height="20" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url={url}&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"></iframe>
+<!-- 
 
 <SoundCloud
   height="165"
   soundcloudLink="{url}&show_artwork=false"
 />
+-->
