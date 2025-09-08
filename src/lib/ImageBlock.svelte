55227fe901525217acdde55414dab9aa298321fd
sportyfred <ivanboring@gmail.com>
2025-09-08 19:20:15 +0200

    Update ImageBlock.svelte

--- src/lib/ImageBlock.svelte
+++ src/lib/ImageBlock.svelte
@@ -6,10 +6,16 @@
 </script>
 
 {#if value.asset}
+{#if value.link}
+<a href={value.link}>
   <figure>
+
+
     <SanityImage image={value} />
     {#if value.caption}
       <figcaption>{value.caption}</figcaption>
     {/if}
   </figure>
+</a>
+{/if}
 {/if}
