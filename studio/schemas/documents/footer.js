7578074051f6cc4fdc021e5db7dce6886936a34f
sportyfred <ivanboring@gmail.com>
2025-09-30 11:32:39 +0200

    Create footer.js

---
+++ studio/schemas/documents/footer.js
@@ -0,0 +1,44 @@
+export default {
+  name: 'footer',
+  type: 'document',
+  title: 'footer',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    {
+      name: 'text',
+      type: 'string',
+      title: 'text'
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      description: 'Some frontends will require a slug to be set to be able to show the person',
+      options: {
+        source: 'name',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image'
+    },
+    {
+      name: 'body',
+      title: 'Post body',
+      type: 'portableText',
+    }
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'slug.current',
+      media: 'image'
+    }
+  }
+}
